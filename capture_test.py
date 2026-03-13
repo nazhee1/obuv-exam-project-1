e72d153466030304916aa43b663c057e463a51e4
nazhee1 <jhggghhhbb@gmail.com>
2026-03-13 15:30:52 +0300

    Исправил экзаменационный проект ООО Обувь

--- capture_test.py
+++
@@ -1,5 +0,0 @@
-
-from app import ShoeStoreApp
-app = ShoeStoreApp()
-app.after(1000, lambda: app.destroy())
-app.mainloop()
--- quick_launch.py
+++
@@ -1,5 +0,0 @@
-from app import ensure_database, ShoeStoreApp
-ensure_database()
-app=ShoeStoreApp()
-app.after(1000, app.destroy)
-app.mainloop()
